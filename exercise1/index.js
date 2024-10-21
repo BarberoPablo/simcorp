fca9c69d152444374d06bdfe90b9443c48dd93e9
Pablo Barbero <pablobarbero220@gmail.com>
2024-10-21 11:30:47 -0300

    first commit

---
+++ exercise1/index.js
@@ -0,0 +1,4 @@
+/* 
+A  boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different. 
+So create a function that returns the total number of boomerangs in an array.
+*/
